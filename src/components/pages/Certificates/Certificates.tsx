2363a62535af8e07701d686f84d743c18e95eb30
Jefersonsteven <gfreycode@gmail.com>
2023-08-11 13:32:40 -0500

    feat: add certificates

--- src/components/pages/Certificates/Certificates.tsx
+++ src/components/pages/Certificates/Certificates.tsx
@@ -2,20 +2,13 @@
 import { ApiCertificates } from '@/app/api/ip'
 import './Certificates.scss'
 import { CertificatesTable } from '@/components/organisms/CertificatesTable/CertificatesTable'
+import { dataCertificates } from './data'
 
 export const Certificates = () => {
   function handleClick () {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const api = ApiCertificates.post('', {
-      data: [
-        {
-          name: 'full stack web developer',
-          url: 'https://certificates.soyhenry.com/new-cert?id=e80263223a951d8dfb622f92864f24bb5723134e2c0ccf23a298e161e0a51ba9',
-          category: 'development',
-          institution: 'henry',
-          date: new Date()
-        }
-      ]
+      data: dataCertificates
     }).then(res => { console.log(res.data) })
   }
 
--- src/components/pages/Certificates/data.ts
+++ src/components/pages/Certificates/data.ts
@@ -9,7 +9,7 @@ export const dataCertificates = [
   {
     name: 'Taller de Inglés Básico para Descripciones Personales',
     url: 'https://platzi.com/p/JefersonSteven/curso/2005-taller-descripciones-personales/diploma/detalle/',
-    category: 'english',
+    category: 'unspecified',
     institution: 'platzi',
     date: new Date('JUL 24 2021')
   },
@@ -86,7 +86,7 @@ export const dataCertificates = [
   {
     name: 'Curso de Inglés Básico A1 para Principiantes',
     url: 'https://platzi.com/p/JefersonSteven/curso/3093-ingles-basico-principiantes/diploma/detalle/',
-    category: 'english',
+    category: 'unspecified',
     institution: 'platzi',
     date: new Date('JAN 12 2023')
   },
@@ -107,7 +107,7 @@ export const dataCertificates = [
   {
     name: 'Curso Gratis de Estrategias para Aprender Inglés en Línea',
     url: 'https://platzi.com/p/JefersonSteven/curso/2633-estrategias-ingles/diploma/detalle/',
-    category: 'english',
+    category: 'unspecified',
     institution: 'platzi',
     date: new Date('APRIL 30 2022')
   },
@@ -170,7 +170,7 @@ export const dataCertificates = [
   {
     name: 'Curso de Optimización del Perfil de LinkedIn',
     url: 'https://platzi.com/p/JefersonSteven/curso/2499-optimizacion-perfil-linkedin/diploma/detalle/',
-    category: 'personal development',
+    category: 'personal_development',
     institution: 'platzi',
     date: new Date('JUN 9 2023')
   },
@@ -205,16 +205,37 @@ export const dataCertificates = [
   {
     name: 'Curso de Introducción a AWS: Fundamentos de Cloud Computing',
     url: 'https://platzi.com/p/JefersonSteven/curso/2731-aws-fundamentos/diploma/detalle/',
-    category: 'cloud',
+    category: 'development',
     institution: 'platzi',
     date: new Date('JUL 9 2023')
   },
   {
-    name: 'Curso Profesional de CSS Grid Layout',
-    url: 'https://platzi.com/p/JefersonSteven/curso/2222-css-grid-layout/diploma/detalle/',
+    name: 'Curso de Inglés Básico A1: Conversaciones Cortas y Habituales',
+    url: 'https://platzi.com/p/JefersonSteven/curso/1945-ingles-principiantes/diploma/detalle/',
+    category: 'unspecified',
+    institution: 'platzi',
+    date: new Date('APR 11 2021')
+  },
+  {
+    name: 'Curso de Gestión Efectiva del Tiempo',
+    url: 'https://platzi.com/p/JefersonSteven/curso/1749-gestion-tiempo/diploma/detalle/',
+    category: 'personal_development',
+    institution: 'platzi',
+    date: new Date('JUL 10 2022')
+  },
+  {
+    name: 'Curso de Fundamentos de TypeScript',
+    url: 'https://platzi.com/p/JefersonSteven/curso/2878-typescript/diploma/detalle/',
     category: 'development',
     institution: 'platzi',
-    date: new Date('MAR 4 2021')
+    date: new Date('APR 2 2023')
+  },
+  {
+    name: 'Curso de Fundamentos de Sass: Crea tu Primera Landing Page',
+    url: 'https://platzi.com/p/JefersonSteven/curso/6867-sass/diploma/detalle/',
+    category: 'development',
+    institution: 'platzi',
+    date: new Date('JUN 28 2023')
   },
   {
     name: 'Curso Profesional de CSS Grid Layout',
@@ -224,52 +245,234 @@ export const dataCertificates = [
     date: new Date('MAR 4 2021')
   },
   {
-    name: 'Curso Profesional de CSS Grid Layout',
-    url: 'https://platzi.com/p/JefersonSteven/curso/2222-css-grid-layout/diploma/detalle/',
+    name: 'Curso de Fundamentos de Node.js',
+    url: 'https://platzi.com/p/JefersonSteven/curso/1759-fundamentos-node/diploma/detalle/',
     category: 'development',
+    institution: 'platzi',
+    date: new Date('MAR 31 2023')
+  },
+  {
+    name: 'Curso de Fundamentos de Diseño de Interfaces UX/UI',
+    url: 'https://platzi.com/p/JefersonSteven/curso/5184-basico-ui/diploma/detalle/',
+    category: 'design',
+    institution: 'platzi',
+    date: new Date('AUGUST 2 2021')
+  },
+  {
+    name: 'Curso de Flujo de Desarrollo Moderno CodeStream',
+    url: 'https://platzi.com/p/JefersonSteven/curso/2157-desarrollo-moderno-codestream/diploma/detalle/',
+    category: 'development',
+    institution: 'platzi',
+    date: new Date('MAY 19 2021')
+  },
+  {
+    name: 'Curso de Frontend Developer',
+    url: 'https://platzi.com/p/JefersonSteven/curso/2467-frontend-developer/diploma/detalle/',
+    category: 'development',
+    institution: 'platzi',
+    date: new Date('JUN 25 2022')
+  },
+  {
+    name: 'Curso Definitivo de HTML y CSS',
+    url: 'https://platzi.com/p/JefersonSteven/curso/2008-html-css/diploma/detalle/',
+    category: 'development',
+    institution: 'platzi',
+    date: new Date('DEC 13 2020')
+  },
+  {
+    name: 'Curso de Figma',
+    url: 'https://platzi.com/p/JefersonSteven/curso/1961-figma/diploma/detalle/',
+    category: 'design',
+    institution: 'platzi',
+    date: new Date('MAY 16 2021')
+  },
+  {
+    name: 'Curso de Estructuras de Datos con JavaScript',
+    url: 'https://platzi.com/p/JefersonSteven/curso/2156-estructuras-datos/diploma/detalle/',
+    category: 'development',
+    institution: 'platzi',
+    date: new Date('FEB 24 2023')
+  },
+  {
+    name: 'Curso de Estrategias para Aprender en Línea Efectivamente 2019',
+    url: 'https://platzi.com/p/JefersonSteven/curso/1608-aprender-2019/diploma/detalle/',
+    category: 'development',
+    institution: 'platzi',
+    date: new Date('APR 26 2021')
+  },
+  {
+    name: 'Curso de Economía Digital: Bitcoin y Criptomonedas',
+    url: 'https://platzi.com/p/JefersonSteven/curso/2452-economia-digital/diploma/detalle/',
+    category: 'personal_development',
+    institution: 'platzi',
+    date: new Date('OCT 23 2021')
+  },
+  {
+    name: 'Curso de ECMAScript: Historia y Versiones de JavaScript',
+    url: 'https://platzi.com/p/JefersonSteven/curso/3504-ecmascript-6/diploma/detalle/',
+    category: 'development',
+    institution: 'platzi',
+    date: new Date('FEB 14 2023')
+  },
+  {
+    name: 'Curso de Diseño Web con CSS Grid y Flexbox',
+    url: 'https://platzi.com/p/JefersonSteven/curso/2229-flexbox-css-grid/diploma/detalle/',
+    category: 'development',
+    institution: 'platzi',
+    date: new Date('MAR 6 2021')
+  },
+  {
+    name: 'Curso de Diseño para Developers',
+    url: 'https://platzi.com/p/JefersonSteven/curso/1906-diseno-programadores/diploma/detalle/',
+    category: 'design',
+    institution: 'platzi',
+    date: new Date('NOV 2 2022')
+  },
+  {
+    name: 'Curso de Debugging con Chrome DevTools',
+    url: 'https://platzi.com/p/JefersonSteven/curso/1867-devtools/diploma/detalle/',
+    category: 'development',
+    institution: 'platzi',
+    date: new Date('OCT 9 2021')
+  },
+  {
+    name: 'Curso de CSS Grid Básico',
+    url: 'https://platzi.com/p/JefersonSteven/curso/2474-css-grid/diploma/detalle/',
+    category: 'development',
+    institution: 'platzi',
+    date: new Date('OCT 26 2022')
+  },
+  {
+    name: 'Curso de Creación de Portafolio para Desarrolladores Web',
+    url: 'https://platzi.com/p/JefersonSteven/curso/6571-portafolio-desarrollo-web/diploma/detalle/',
+    category: 'personal_development',
+    institution: 'platzi',
+    date: new Date('JUL 30 2023')
+  },
+  {
+    name: 'Curso de Creación de CV',
+    url: 'https://platzi.com/p/JefersonSteven/curso/6603-cv/diploma/detalle/',
+    category: 'personal_development',
+    institution: 'platzi',
+    date: new Date('JUN 1 2023')
+  },
+  {
+    name: 'Curso de Computación Básica',
+    url: 'https://platzi.com/p/JefersonSteven/curso/1741-computacion-basica-2019/diploma/detalle/',
+    category: 'development',
+    institution: 'platzi',
+    date: new Date('MAY 10 2021')
+  },
+  {
+    name: 'Curso de Closures y Scope en JavaScript',
+    url: 'https://platzi.com/p/JefersonSteven/curso/3213-javascript-closures-scope/diploma/detalle/',
+    category: 'development',
+    institution: 'platzi',
+    date: new Date('JAN 28 2023')
+  },
+  {
+    name: 'Curso de Backend con Node.js: API REST con Express.js',
+    url: 'https://platzi.com/p/JefersonSteven/curso/2485-backend-nodejs/diploma/detalle/',
+    category: 'development',
+    institution: 'platzi',
+    date: new Date('MAR 30 2023')
+  },
+  {
+    name: 'Curso de Asincronismo con JavaScript',
+    url: 'https://platzi.com/p/JefersonSteven/curso/3175-asincronismo-js/diploma/detalle/',
+    category: 'development',
+    institution: 'platzi',
+    date: new Date('MAR 6 2023')
+  },
+  {
+    name: 'Curso de Arquitecturas CSS',
+    url: 'https://platzi.com/p/JefersonSteven/curso/7991-arquitecturas-css/diploma/detalle/',
+    category: 'development',
+    institution: 'platzi',
+    date: new Date('JUL 29 2023')
+  },
+  {
+    name: 'Curso de Aprendizaje en Línea para Cumplir tus Objetivos Profesionales',
+    url: 'https://platzi.com/p/JefersonSteven/curso/2643-aprender-2021/diploma/detalle/',
+    category: 'personal_development',
+    institution: 'platzi',
+    date: new Date('JAN 19 2023')
+  },
+  {
+    name: 'Curso de Animaciones con CSS',
+    url: 'https://platzi.com/p/JefersonSteven/curso/2337-animaciones-css/diploma/detalle/',
+    category: 'development',
+    institution: 'platzi',
+    date: new Date('MAY 31 2021')
+  },
+  {
+    name: 'Curso Básico de Tailwind 2 y 3',
+    url: 'https://platzi.com/p/JefersonSteven/curso/2702-tailwind/diploma/detalle/',
+    category: 'development',
+    institution: 'platzi',
+    date: new Date('APR 19 2023')
+  },
+  {
+    name: 'Curso Básico de Python',
+    url: 'https://platzi.com/p/JefersonSteven/curso/1937-python-basico/diploma/detalle/',
+    category: 'development',
+    institution: 'platzi',
+    date: new Date('MAY 23 2021')
+  },
+  {
+    name: 'Curso Básico de Programación Orientada a Objetos con JavaScript',
+    url: 'https://platzi.com/p/JefersonSteven/curso/2332-javascript-poo/diploma/detalle/',
+    category: 'development',
+    institution: 'platzi',
+    date: new Date('JUL 24 2021')
+  },
+  {
+    name: 'Curso Básico de Programación con C#',
+    url: 'https://platzi.com/p/JefersonSteven/curso/3086-csharp/diploma/detalle/',
+    category: 'development',
+    institution: 'platzi',
+    date: new Date('JUL 1 2023')
+  },
+  {
+    name: 'Curso Básico de JavaScript',
+    url: 'https://platzi.com/p/JefersonSteven/curso/1814-basico-javascript/diploma/detalle/',
+    category: 'development',
+    institution: 'platzi',
+    date: new Date('MAR 12 2021')
+  },
+  {
+    name: 'Curso Básico de Computadores e Informática',
+    url: 'https://platzi.com/p/JefersonSteven/curso/2793-computacion-basica/diploma/detalle/',
+    category: 'development',
+    institution: 'platzi',
+    date: new Date('DEC 18 2022')
+  },
+  {
+    name: 'Audiocurso de Historia de Bitcoin y Criptomonedas',
+    url: 'https://platzi.com/p/JefersonSteven/curso/2443-historia-bitcoin/diploma/detalle/',
+    category: 'personal_development',
     institution: 'platzi',
     date: new Date('MAR 4 2021')
   },
   {
-    name: 'Curso Profesional de CSS Grid Layout',
-    url: 'https://platzi.com/p/JefersonSteven/curso/2222-css-grid-layout/diploma/detalle/',
-    category: 'development',
+    name: 'Audiocurso de Gestión Emocional de Inversión en Criptomonedas',
+    url: 'https://platzi.com/p/JefersonSteven/curso/2553-emociones-cripto/diploma/detalle/',
+    category: 'personal_development',
     institution: 'platzi',
     date: new Date('MAR 4 2021')
   },
   {
-    name: 'Curso Profesional de CSS Grid Layout',
-    url: 'https://platzi.com/p/JefersonSteven/curso/2222-css-grid-layout/diploma/detalle/',
-    category: 'development',
-    institution: 'platzi',
-    date: new Date('MAR 4 2021')
+    name: 'Ingles Básico A1',
+    url: 'https://www.efset.org/cert/QbLWKF',
+    category: 'unspecified',
+    institution: 'EF ILC',
+    date: new Date('JUN 5 2023')
   },
   {
-    name: 'Curso Profesional de CSS Grid Layout',
-    url: 'https://platzi.com/p/JefersonSteven/curso/2222-css-grid-layout/diploma/detalle/',
+    name: 'Introducción Al Desarrollo Web I',
+    url: 'https://learndigital.withgoogle.com/activate/validate-certificate-code',
     category: 'development',
-    institution: 'platzi',
-    date: new Date('MAR 4 2021')
-  },
-  {
-    name: 'Curso Profesional de CSS Grid Layout',
-    url: 'https://platzi.com/p/JefersonSteven/curso/2222-css-grid-layout/diploma/detalle/',
-    category: 'development',
-    institution: 'platzi',
-    date: new Date('MAR 4 2021')
-  },
-  {
-    name: 'Curso Profesional de CSS Grid Layout',
-    url: 'https://platzi.com/p/JefersonSteven/curso/2222-css-grid-layout/diploma/detalle/',
-    category: 'development',
-    institution: 'platzi',
-    date: new Date('MAR 4 2021')
-  },
-  {
-    name: 'Curso Profesional de CSS Grid Layout',
-    url: 'https://platzi.com/p/JefersonSteven/curso/2222-css-grid-layout/diploma/detalle/',
-    category: 'development',
-    institution: 'platzi',
-    date: new Date('MAR 4 2021')
+    institution: 'google activate',
+    date: new Date('JUN 10 2023')
   }
 ]
