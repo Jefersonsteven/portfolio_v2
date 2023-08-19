1bb2bb339d40ad431214baafb86e95271cb3028f
Jefersonsteven <gfreycode@gmail.com>
2023-08-19 10:19:54 -0500

    feat: add mini project 🤩

--- src/components/pages/Certificates/data.ts
+++ src/components/pages/Certificates/data.ts
@@ -1,3 +1,9 @@
 export const dataCertificates = [
-  {}
+  {
+    name: 'Curso de React.js',
+    url: 'https://platzi.com/p/JefersonSteven/curso/7395-react/diploma/detalle/',
+    category: 'development',
+    institution: 'platzi',
+    date: new Date()
+  }
 ]
